53dd5365578eef1a822b8cb81ecc69e7de0eab29
osvaldoneto0 <osvaldonetoo011@gmail.com>
2024-06-24 18:43:30 -0300

    Props e hooks

--- blogpessoal/src/App.tsx
+++ blogpessoal/src/App.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
 import Home from './assets/paginas/home/Home';
 import './App.css';
-
+import React from 'react';
 
 function App() {
+ 
   return (
-   <Home />
+    <>
+      <Home/>
+    </>
   );
 }
 
--- blogpessoal/src/assets/paginas/home/Home.tsx
+++ blogpessoal/src/assets/paginas/home/Home.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import './Home.css';
+import React, { useState } from 'react';
 
-function Home(){
-    return (
-        <>
-            <h1 className="titulo">Home</h1>
-            <img src="https://tm.ibxk.com.br/2023/09/26/26121304655146.jpg?ims=1200x675" alt="Imagem Tela Inicial" className="img"/>
-        </>
-    );
+function Home() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  return (
+    <div>
+      {loggedIn ? (
+        <h1>Bem-vindo de volta!</h1>
+      ) : (
+        <button onClick={() => setLoggedIn(true)}>Entrar</button>
+      )}
+    </div>
+  );
 }
-
 export default Home;
