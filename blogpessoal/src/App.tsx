f6a295f8102e0040e0493eb1409e6b2e77867300
osvaldoneto0 <osvaldonetoo011@gmail.com>
2024-06-23 21:34:28 -0300

    primeiro projeto com react

---
+++ blogpessoal/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from './assets/paginas/home/Home';
+import './App.css';
+
+
+function App() {
+  return (
+   <Home />
+  );
+}
+
+export default App;
---
+++ blogpessoal/src/assets/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://tm.ibxk.com.br/2023/09/26/26121304655146.jpg?ims=1200x675" alt="Imagem Tela Inicial" className="img"/>
+        </>
+    );
+}
+
+export default Home;
